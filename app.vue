69857318a2ef06e9092751f1dc23724383460ec6
therollingambit <therollingambit@gmail.com>
2023-05-07 01:56:22 +0800

    adding pages & routes

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <p>Hello, ninjas!</p>
-  </div>
-</template>
---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nam qui
+      perspiciatis voluptatum doloribus quibusdam amet ab. Dolore, atque
+      corporis?
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
