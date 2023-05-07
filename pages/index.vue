531a215983d1eebe9299118012448c16254ea5d3
therollingambit <therollingambit@gmail.com>
2023-05-07 15:09:07 +0800

    NuxtLink in navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/" >Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
     <h2>Home</h2>
     <p>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nam qui
@@ -12,11 +23,11 @@
 <script setup></script>
 
 <style scoped>
-  h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
-  p {
-    margin: 20px 0;
-  }
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
 </style>
