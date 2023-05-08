cf58e0550dc58b9bda3ab3f3b8d3ba9558479833
therollingambit <therollingambit@gmail.com>
2023-05-08 22:34:03 +0800

    reusable components

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,13 +2,15 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products">
-        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+        <ProductCard :product="p" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import ProductCard from '~/components/ProductCard.vue';
+
   definePageMeta({
     layout: 'products',
   })
