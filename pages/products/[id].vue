de7a0d41e42c9a5017c03821afb2a76ec6e79730
therollingambit <therollingambit@gmail.com>
2023-05-08 23:45:55 +0800

    error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -16,6 +16,10 @@ const uri = 'https://fakestoreapi.com/products/' + id
 
 // fetch the product
 const { data: product } = await useFetch(uri, { key: id }) // refetch data coz key is diff
+
+if (!product.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+}
 </script>
 
 <style scoped></style>
